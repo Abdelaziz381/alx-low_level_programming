ce60f8938ff1b2059a32eac1446abf33ce9fd424
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-07 15:24:38 -0700

    positive or negativr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is zore\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is nigative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
