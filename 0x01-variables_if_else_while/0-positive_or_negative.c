40b71165744dbb178ca158606dae575c19e6f749
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-07 15:20:34 -0700

    positive or negativr

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("is positive \n");
+		printf("%d is positive \n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zore \n");
+		printf("%d is zore \n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is nigative \n");
+		printf("%d is nigative \n", n);
 	}
 	return (0);
 }
