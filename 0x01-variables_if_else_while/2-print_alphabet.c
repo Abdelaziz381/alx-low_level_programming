82f4474e199aaf1321608b4dca0061cd06081ade
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-07 17:39:00 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	char char_1 ,char_2 = '\n';
-	for (char_1 = 'a'; char_1 <= 'z' ; char_1++)
+	char char_1[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i
+	for (i = 0; i < 26 ; i++)
 	{
-		putchar(char_1);
-		putchar(char_2);
+		putchar(char_1[i]);
 	}
+	putchar('\n');
 	return (0);
 }
