ac8e6dad667c8a2597e1da457f9c486b339eee1c
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-08 14:17:27 -0700

    adding a copy of 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char char_1[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26 ; i++)
+	{
+		putchar(char_1[i]);
+	}
+	putchar('\n');
+	return (0);
+}
