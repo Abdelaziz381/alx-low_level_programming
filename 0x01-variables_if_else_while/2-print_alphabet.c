4e5fe9cd778342c911e0b74563eaf5c5b205de1f
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-08 14:22:41 -0700

    copy of 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-	char char_1[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char char_1, char_2 = '\n';
 
-	for (i = 0; i < 26 ; i++)
+	for (char_1 = 'a'; char_1 < 'z' ; char_1++)
 	{
-		putchar(char_1[i]);
+		putchar(char_1);
 	}
-	putchar('\n');
+	putchar(char_2);
 	return (0);
 }
