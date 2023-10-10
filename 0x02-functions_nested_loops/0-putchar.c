2436adb7af489ca1544448056e8f41028cd2d7a3
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-10 13:41:02 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Prints _putchar.
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-
-	printf("_putchar\n");
-	return (0);
+void _putchar(char c) {
+  write(1, &c, 1);
 }
+
+int main()
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u'); 
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
