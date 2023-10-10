177b02f869aac3f5633184764c8d217d47b62ac0
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-10 14:45:03 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
-/*sdasd*
+/**
  * main - Prints _putchar.
  * Return
- * 
  */
-
 int main()
+{
+	char var[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
 	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u'); 
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+		_putchar(var[i]);
+	}
+	_putchar(10);
 	return (0);
 	}
