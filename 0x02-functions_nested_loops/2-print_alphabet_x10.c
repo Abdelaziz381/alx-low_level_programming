ca7852615d21264af5d0cdd28ff9543d073bf8b8
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-10 15:54:30 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet
- * in lowercase
+ * print_alphabet_x10 - Print alphabet 10 times
+ * 
  * Return: no return 0
  */
 
@@ -10,11 +10,13 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	int var;
+	
 	while (i++ <= 9)
 	{
 		for (var = 'a'; var <= 'z'; var++)
+		{
 			_putchar(var);
-
+		}
 		_putchar(10);
 	}
 }
