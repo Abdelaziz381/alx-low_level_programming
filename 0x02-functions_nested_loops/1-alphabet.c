b178f9ef4aee761e117c82c4ab91ff57f082872f
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-10 15:30:58 -0700

    b

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *print_alphabet - Print alphabet
- *in lowercase
- *Return: no return 0 
+ * print_alphabet - Print alphabet
+ * in lowercase
+ * Return: no return 0
  */
 
 void print_alphabet(void)
