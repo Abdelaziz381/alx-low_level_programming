edc346c719ee613c084efaa81820941e80584bfe
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-12 16:44:15 -0700

    print_most numbers , more numbers , print lines , print diagonal

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,24 @@
 #include "main.h"
 /**
-*print_line - prints a straight line
-*@n:parameter
-*Return:returns nothing
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
 */
-void print_line(int n)
+void more_numbers(void)
 {
-while (n-- > 0)
+int number1, number2;
+
+for (number1 = 0; number1 < 10; number1++)
 {
-_putchar('_');
+for (number2 = 0; number2 <= 14; number2++)
+
+{
+if (number2 > 9)
+{
+_putchar((number2 / 10) + '0');
 }
-_putchar('\n');
+_putchar((number2 % 10) + '0');
+}
+_putchar(10);
+}
 }
