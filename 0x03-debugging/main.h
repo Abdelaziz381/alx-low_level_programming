e06e2c832cf794669942c472b75a82579030174d
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-12 15:17:57 -0700

    check positive or negative function

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/*Task 0. Multiple mains */
+void positive_or_negative(int i);
+
+/*Task 1. Like, comment, subscribe*/
+int largest_number(int, int, int);
+
+/*Task 2. 0 > 972?*/
+void print_remaining_days(int, int, int);
+
+/*3. Leap year*/
+int convert_day(int, int);
+
+#endif 
