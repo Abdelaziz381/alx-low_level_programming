c33fada71f0966c9d500580ebc97c1faba71b0fc
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-16 18:01:03 -0700

    prints a string, in reverse, followed by a new line.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,12 @@
  * print_rev : prints a string, in reverse, followed by a new line.
  *
  * @str : paramter
- * On success: returns string in reverse order followe by new line
  */
 
 void print_rev(char *str)
 {
 	int len = 0, index = 0;
-	
+
 	len  = _strlen(str);
 	index = len - 1;
 
