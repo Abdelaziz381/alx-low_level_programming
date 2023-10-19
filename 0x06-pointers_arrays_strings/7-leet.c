92c13dfea5dda608a18c944af30087e2ad110989
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-19 14:07:00 -0700

    0x06 task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,14 +1,12 @@
 #include "main.h"
-
 /**
 * leet - function that encode a string
-*@str: string that will be encoded
+*@str:string that will be encoded
 *Return:returns encoded string
 */
 
 char *leet(char *str)
 {
-	
 	int index1 = 0, index2;
 
 	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
