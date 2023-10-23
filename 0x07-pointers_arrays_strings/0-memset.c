d2b77d27ced9d8e3fb6bc8f3b99e40d1c2d53d29
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-23 15:56:46 -0700

    0x07

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,15 +2,15 @@
 
 /**
  * _memset - a function that fills memory with a constant byte
- * @*s: a pointer to b
+ * @s: a pointer to b
  * @b: the value of the function
  * @n: increment operator
- * Return : type of char pointer
+ * Return : type of char pointer *s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	for(unsigned int i = 0; i < n ; i++)
+	for( unsigned int i = 0; i < n ; i++ )
 	{
 		s[i] = b;
 	}
