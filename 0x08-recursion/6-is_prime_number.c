750431d131668901b009fb909a3b886477ed09e1
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-25 14:13:30 -0700

    0x08 - 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * check_prime - checks to see if number is prime
+ * @x: int
+ * @y: int
+ * Return:int
+ */
+int check_prime(int x, int y)
+{
+	if (y < 2 || y % x == 0)
+		return (0);
+	else if (x > y / 2)
+		return (1);
+	else
+		return (check_prime(x + 1, y));
+}
+
+/**
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (check_prime(2, n));
+}
