f174a0e9643ccdf0b12ce9a7ee8771c9dfe90ffc
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-10-31 15:35:52 -0700

    0x0B

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,7 +39,7 @@ char **strtow(char *str)
 	if (str == NULL || *str == '\0')
 		return (NULL);
 
-	num = c_words(str);
+	num = c_word(str);
 	if (num == 1)
 		return (NULL);
 
@@ -55,7 +55,8 @@ char **strtow(char *str)
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-				j++;
+				;
+			j++;
 			w[c] = (char *)malloc(j * sizeof(char));
 			j--;
 			if (w[c] == NULL)
