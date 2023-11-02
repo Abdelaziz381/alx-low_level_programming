46082b277b1ee7b05b784fe9deab948315a4243e
root <root@Abdelaziz-pc>
2023-11-03 00:33:13 +0200

    0x0C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using
+ * @b: int
+ * Return : if succsess return a pointer to allocated memory
+ * on fail termination with a status value of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	
+	if *ptr != NULL
+		exit(98);
+}
