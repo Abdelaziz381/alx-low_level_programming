add66a042e8c4b92dbe29727dea564cd88abd4ed
Abdelaziz <abdelaziz38elsawy@gmail.com>
2023-11-07 02:12:36 +0200

    0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - new dog struction
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 struct dog
 {
-	char* name;
-	float* age;
-	char* owner;
+	char *name;
+	float age;
+	char *owner;
 }
 
 #endif /* DOG_H */
