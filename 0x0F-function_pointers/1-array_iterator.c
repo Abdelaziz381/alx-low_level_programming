597ca90d44f7255270faa14f78ba79fc71d08d08
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-11-08 15:45:52 -0800

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end_of_ar = array + size -1;
+	int *end_of_ar = array + size - 1;
 
 	if (array && size && action)
 	{
