456a83fe926280b8a32dedf18131fa51eea4a6ff
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-11-21 11:46:38 +0200

    0x13 task 0 % 1 fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,12 +5,11 @@
  * @h: pointer to first node
  * Return: size of list
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
