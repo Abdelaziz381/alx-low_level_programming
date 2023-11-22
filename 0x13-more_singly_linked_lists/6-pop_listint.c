c060e66ede737a1c07a09562012c15d20aacca87
AbdelazizElsawy <abdelaziz38elsawy@gmail.com>
2023-11-22 02:00:06 +0200

    6 : 10 tasks

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop head node list
+ * @head: pointer address of first node
+ * Return: value of popped node
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || *head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
